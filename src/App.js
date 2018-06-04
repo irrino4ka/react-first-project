4a60004328d6ee60839ec9f3ca05d4e1dbc9017d
irrino4ka <ir.rino4ka@gmail.com>
2018-06-04 19:12:29 +0300

    delete func

--- src/App.js
+++ src/App.js
@@ -5,23 +5,13 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age: 21},
-      {name: 'Anna', age: 13},
-      {name: 'Lolla', age: 11},
-      {name: 'Billy', age: 10}
+      {id: '1', name: 'Max', age: 21},
+      {id: '2', name: 'Anna', age: 13},
+      {id: '3', name: 'Lolla', age: 11},
+      {id: '4', name: 'Billy', age: 10}
     ],
     otherState: 'other value',
     showPersons: false
-  }
-
-  swithcNameHandler = (newName) => {
-    this.setState({persons:
-      [
-        {name: newName, age: 21},
-        {name: 'An', age: 23},
-        {name: 'Lolla', age: 11},
-        {name: 'Billy', age: 100}
-      ] }) 
   }
 
   nameChangedHandler = (event) => {
@@ -37,7 +27,12 @@ class App extends Component {
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow});
   }
-
+  deletePersonHandler = (personIndex) =>{
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
 
   render() {
     const style= {
@@ -49,31 +44,28 @@ class App extends Component {
     }
 
     let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+            <div >
+              {this.state.persons.map((person, key) => {
+                return <Person 
+                click = {() => this.deletePersonHandler(key)} // this.deletePersonHandler.bind(this, key)
+                name = {person.name}
+                age={person.age}
+                key={person.id}/>
+              })}
+            </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>I am react file</h1>
         <button 
           style= {style}
           onClick={this.togglePersonHandler} >Toggle Persons</button>
-        {
-          this.state.showPersons ? 
-            <div >
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age}/>
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}/>
-              <Person
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age}
-                click = {this.swithcNameHandler.bind(this, 'Rose')}
-                changed ={this.nameChangedHandler}>My hobbies: Racing</Person>
-              <Person 
-                name={this.state.persons[3].name} 
-                age={this.state.persons[3].age}/>
-            </div> : null
-        }
+        {persons}
       </div>
     );
   }
