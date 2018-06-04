658fd415367502350207227bb728516446aee951
irrino4ka <ir.rino4ka@gmail.com>
2018-06-04 18:28:36 +0300

    added styling

--- src/App.js
+++ src/App.js
@@ -34,10 +34,19 @@ class App extends Component {
 
 
   render() {
+    const style= {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>I am react file</h1>
-        <button onClick={() => this.swithcNameHandler('Laura')} >Switch name</button>
+        <button 
+          style= {style}
+          onClick={() => this.swithcNameHandler('Laura')} >Switch name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div onClick={props.click}>
+        <div onClick={props.click} className='Person'>
         <p>I am {props.name} a Person! {props.age}</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
